85a56b6142f46cb575d0715f2ac35758d1a757ee
Alexandre Errasti <alexandreerrasti@MacBook-Air-de-Alexandre.local>
2025-05-31 22:27:16 +0700

    Import propre sans clé API

---
+++ frontend/run-netlify-test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Script pour lancer Netlify dev avec les variables d'environnement
+
+# Exporter les variables d'environnement nécessaires pour les fonctions Netlify
+
+ Lancer Netlify dev
+echo "Démarrage de Netlify dev avec les variables d'environnement configurées..."
+netlify dev
+
+
